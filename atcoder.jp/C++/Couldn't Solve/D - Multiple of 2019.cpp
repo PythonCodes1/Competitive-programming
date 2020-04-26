36cdba3a2594034e6d3ee9d072a437afefc2f391
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-04-26 10:19:55 -0400

    Create D - Multiple of 2019.cpp

---
+++ atcoder.jp/C++/Couldn't Solve/D - Multiple of 2019.cpp
@@ -0,0 +1,3 @@
+// Given is a string S consisting of digits from 1 through 9. 
+// Find the number of pairs of integers ( i , j ) ( 1 ≤ i ≤ j ≤ | S | ) that satisfy the following condition: 
+// Condition: In base ten, the i -th through j -th characters of S form an integer that is a multiple of 2019.
