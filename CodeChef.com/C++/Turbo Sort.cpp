7891952c6629098e90cbc06684880b231fee73a1
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-04-29 11:28:58 -0400

    Create Turbo Sort.cpp

---
+++ CodeChef.com/C++/Turbo Sort.cpp
@@ -0,0 +1,25 @@
+/*
+Given the list of numbers, you are to sort them in non decreasing order.
+
+https://www.codechef.com/problems/TSORT
+*/
+
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	int A[t];
+	int x = 0;
+    for(int i = 0; i < t; i++){
+        cin >> x;
+        A[i] = x;
+    }
+    sort(A,A+t);
+    for(int i = 0; i < t; i++){
+        cout << A[i] << endl;
+    }
+	return 0;
+}
