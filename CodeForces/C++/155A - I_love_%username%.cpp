38d62e0026f602896ecbd1f3a4ca73322d514a61
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-05-01 17:41:58 -0400

    Create 155A - I_love_%username%.cpp

---
+++ CodeForces/C++/155A - I_love_%username%.cpp
@@ -0,0 +1,28 @@
+// https://codeforces.com/problemset/problem/155/A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, points;
+    cin >> n >> points;
+    int min(points), max(points), amazing(0);
+    while (--n)
+    {
+        cin >> points;
+        if (points < min)
+        {
+            min = points;
+            ++amazing;
+        }
+        if (points > max)
+        {
+            max = points;
+            ++amazing;
+        }
+    }
+    cout << amazing << endl;
+    return 0;
+}
