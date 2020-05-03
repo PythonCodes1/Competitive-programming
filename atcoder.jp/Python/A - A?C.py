40a4c65f7dfbbfbb88f08ae2e6596acea13695a9
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-05-03 09:57:15 -0400

    Create A - A?C.py

---
+++ atcoder.jp/Python/A - A?C.py
@@ -0,0 +1,17 @@
+"""
+Problem Statement
+AtCoder Inc. holds a contest every Saturday.
+
+There are two types of contests called ABC and ARC, and just one of them is held at a time.
+
+The company holds these two types of contests alternately: an ARC follows an ABC and vice versa.
+
+Given a string 
+S
+ representing the type of the contest held last week, print the string representing the type of the contest held this week.
+"""
+
+if input() == "ARC":
+    print("ABC")
+else:
+    print("ARC")
