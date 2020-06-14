d68031f1f9d4e806972b8150bb84ae95ca76a077
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-06-14 15:31:05 -0400

    Create 10 - Lucky Four.cpp

---
+++ CodeChef.com/C++/Beginner/10 - Lucky Four.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Get the length of the list
+    int t;
+    cin >> t;
+    // Create a while loop that scans the list
+    while(t--){
+        // Going through each list
+        int count=0, a;
+        cin >> a;
+        // Another while loop to go through each digit to see if it's a 4
+        while(a>1){
+            int a1;
+            a1 = a%10;
+            if(a1==4){
+                count++;
+            }
+            a /= 10;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
