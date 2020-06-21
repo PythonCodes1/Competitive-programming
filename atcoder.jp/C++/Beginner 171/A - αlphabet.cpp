ff8bdf24a7aaff4a3a7284a5cba554d28d13e5fe
PythonCodes1 <59237812+PythonCodes1@users.noreply.github.com>
2020-06-21 11:32:45 -0400

    Create A - αlphabet.cpp

---
+++ atcoder.jp/C++/Beginner 171/A - αlphabet.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    char t;
+    cin >> t;
+    if (isupper(t))
+        cout << "A" << endl;
+    else
+        cout << "a" << endl;
+}
